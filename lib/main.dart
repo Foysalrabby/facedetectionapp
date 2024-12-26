7d7a1d1fe3b39ac98612234303a4631eff84ed62
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-12-26 16:40:47 +0600

    start face detection app

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:facedetectionapp/facedetectionmain.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Facedetection(), // Reference to your home widget
+
+    ),
+  );
+}
+
